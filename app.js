358ac52e879b16b748e24a41eec32eed46da2723
Carlos Grisales <41649792+carlos0320@users.noreply.github.com>
2021-10-21 16:49:47 -0500

    upd app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
 
